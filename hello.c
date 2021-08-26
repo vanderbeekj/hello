50c4852dfe4748ff345b36c39f03164e30eab850
Joseph Vanderbeek <jvanderbeek4@huskers.unl.edu>
2021-08-26 17:48:06 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+ /**
+ * Author: Joseph Vanderbeek
+ * Date: 2021/08/26
+ *
+ * A simple hello world program in C
+ *
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+
+     printf("Hellow World!\n");
+
+     return 0;
+ }
